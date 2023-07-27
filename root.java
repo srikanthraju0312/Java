270840988088f9c60158b653db7904e1095a424e
unknown <srikanthraju.n@axiomio.com>
2023-07-27 20:58:35 +0530

    importing packages

---
+++ root.java
@@ -0,0 +1,14 @@
+//This program returns the square root of a number
+//Should import math package to do square roots
+import java.lang.Math;
+class root
+{
+    public static void main(String[] args)
+    {
+        //declaring variables
+        double x = 4;
+        double y;
+        y = Math.sqrt(x);
+        System.out.println("The square root of 4 is"+y);
+    }
+}
